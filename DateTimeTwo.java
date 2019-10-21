9ef2cdb5b0f526740984aec2e590ff212f5ce4d5
Rachel Bennett <rachel.l.bennett-1@ou.edu>
2019-10-21 18:31:30 -0500

    Added skeleton code for DateTimeTwo

--- DateTimeTwo.java
+++ DateTimeTwo.java
@@ -1,4 +1,24 @@
 
 public class DateTimeTwo {
 
+	public String daysOfCurrentMonth() {
+		//TODO: Return the days of this Month
+	}
+	
+	public String daysOfAnyMonth(int month, int year) {
+		//TODO: Return fifteenth and last day of month calculated for any month
+	}
+	
+	public String compareYear() {
+		//TODO: Read in contents of Dates.txt, then see if it is a leap year
+		//Calculate difference between today's date and the given date. Save these dates in a hashmap
+	}
+	
+	public String dateHashMap() {
+		//TODO: Print out hash map
+	}
+	
+	public String dateHashMapSorted() {
+		//TODO: Sort and print hash map
+	}
 }
